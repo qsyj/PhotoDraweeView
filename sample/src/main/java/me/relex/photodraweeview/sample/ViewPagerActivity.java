297eea5fe705fa6f32982671aeff1bfcb8a313cb
qsyj <w_qing_lin@163.com>
2017-08-25 16:19:42 +0800

    功能实现

---
+++ sample/src/main/java/me/relex/photodraweeview/sample/ViewPagerActivity.java
@@ -0,0 +1,102 @@
+package me.relex.photodraweeview.sample;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeControllerBuilder;
+import com.facebook.drawee.controller.BaseControllerListener;
+import com.facebook.imagepipeline.image.ImageInfo;
+import me.relex.circleindicator.CircleIndicator;
+import me.relex.photodraweeview.OnPhotoTapListener;
+import me.relex.photodraweeview.PhotoDraweeView;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    public static void startActivity(Context context) {
+        context.startActivity(new Intent(context, ViewPagerActivity.class));
+    }
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewpager);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setNavigationOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                onBackPressed();
+                            }
+                        });
+
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        MultiTouchViewPager viewPager = (MultiTouchViewPager) findViewById(R.id.view_pager);
+        viewPager.setAdapter(new DraweePagerAdapter());
+        indicator.setViewPager(viewPager);
+    }
+
+    public class DraweePagerAdapter extends PagerAdapter {
+
+        private int[] mDrawables = new int[] {
+                R.drawable.viewpager_1, R.drawable.viewpager_2, R.drawable.viewpager_3
+        };
+
+        @Override public int getCount() {
+            return mDrawables.length;
+        }
+
+        @Override public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override public Object instantiateItem(ViewGroup viewGroup, int position) {
+            final PhotoDraweeView photoDraweeView = new PhotoDraweeView(viewGroup.getContext());
+            PipelineDraweeControllerBuilder controller = Fresco.newDraweeControllerBuilder();
+            controller.setUri(Uri.parse("res:///" + mDrawables[position]));
+            controller.setOldController(photoDraweeView.getController());
+            controller.setControllerListener(new BaseControllerListener<ImageInfo>() {
+                @Override
+                public void onFinalImageSet(String id, ImageInfo imageInfo, Animatable animatable) {
+                    super.onFinalImageSet(id, imageInfo, animatable);
+                    if (imageInfo == null) {
+                        return;
+                    }
+                    photoDraweeView.update(imageInfo.getWidth(), imageInfo.getHeight());
+                }
+            });
+            photoDraweeView.setController(controller.build());
+            photoDraweeView.setOnPhotoTapListener(new OnPhotoTapListener() {
+                @Override
+                public void onPhotoTap(View view, float x, float y) {
+                    if (mToolbar.getVisibility() == View.VISIBLE) {
+                        mToolbar.setVisibility(View.INVISIBLE);
+                    } else {
+                        mToolbar.setVisibility(View.VISIBLE);
+                    }
+                }
+            });
+
+            try {
+                viewGroup.addView(photoDraweeView, ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return photoDraweeView;
+        }
+    }
+}
